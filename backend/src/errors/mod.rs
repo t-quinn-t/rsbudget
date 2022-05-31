5b8c29d0aa4803cb0c6ecd656cb9431219cbc44c
Quinn <qintutao@gmail.com>
2022-05-30 18:02:28 -0700

    dev: added postgresql support, but has a bit problem with user/pass

--- backend/src/errors/mod.rs
+++ backend/src/errors/mod.rs
@@ -2,8 +2,10 @@
 /// Convert different error types to a custom error
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
-    #[error("Database Error Occurred.")] 
+    #[error(transparent)] 
     DBError(#[from] sqlx::Error),
-    #[error("Environment Variables Loading Failed.")]
-    EVError(#[from] std::io::Error),
+    #[error(transparent)]
+    IOError(#[from] std::io::Error),
+    #[error("Failed reading .env values")]
+    EVError(#[from] std::env::VarError)
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,20 @@
-use dotenv;
 use backend::errors::Error;
+
+use dotenv;
+
+use sqlx::Pool;
+use sqlx::postgres::Postgres;
+use sqlx::query;
 
 #[async_std::main]
 async fn main() -> Result<(), Error> {
     dotenv::dotenv().ok();
-    let db_url = std::env::var("DATABASE_URL").unwrap();
+
+    // Config database
+    let db_url = std::env::var("DATABASE_URL")?;
+    dbg!(db_url);
+    let mut db_pool: Pool<Postgres> = Pool::connect(&db_url).await?;
+    let rows = query!("SELECT 1 AS one").fetch_one(&mut db_pool).await?;
 
     // Setup server
     let mut app = tide::new();

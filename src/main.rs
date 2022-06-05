e8af26f095956fe7066f972dcbb63d6b3ac8cc63
Quinn <qintutao@gmail.com>
2022-06-05 15:38:18 -0700

    dev: added tui and crossterm frontend

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
+use std::io;
+use tui::{Terminal, backend::CrosstermBackend}; 
+
+use rbudget::errors::Error;
+
 extern crate log;
 extern crate pretty_env_logger;
 
-fn main() {
+fn main() -> Result<(), Error> {
     pretty_env_logger::init();
+    
+    // Init tui application
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    
+    Ok(())
 }
 
 
